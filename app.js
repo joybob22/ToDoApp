ccfcb7763db3c7d9b651c63bf0bbcca24b769e56
joybob22 <braydenlemke@yahoo.com>
2016-12-21 08:52:05 -0700

    made simple way of using ui-router to figure it out

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 /**
  * Created by braydenlemke on 12/21/16.
  */
+(function () {
+
+    var app = angular.module("todoApp", ["ui.router"]);
+
+    app.config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("home");
+
+        /**
+         * add a home state and a todo state using stateProvider
+         */
+
+        $stateProvider
+
+            .state("home", {
+                url: "/home",
+                templateUrl: "home.html"               /**   add home template   */
+            })
+
+            .state("todo", {
+                url: "/todo",
+                templateUrl: "todo.html"               /**   add todo template   */
+            })
+
+    })
+
+})();
